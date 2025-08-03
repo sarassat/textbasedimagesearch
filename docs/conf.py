7f3f92c3ff815ea53c9fab3cc706144b9bc5ff37
sarassat <saraswatsatya1@gmail.com>
2025-08-03 14:02:13 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-project = 'TextBasedImageSearch'
+project = 'Text_Based_Image_Retrieval'
 author = 'Nitin Saraswat'
 release = '0.1'
 
