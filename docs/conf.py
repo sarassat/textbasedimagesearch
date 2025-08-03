2e7163e5f3f9d6f6cbea4ed45744c39ef31aa707
sarassat <saraswatsatya1@gmail.com>
2025-08-03 14:01:51 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,18 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'TextBasedImageSearch'
+author = 'Nitin Saraswat'
+release = '0.1'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'alabaster'
 
